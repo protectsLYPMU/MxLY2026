46156fb6c0ca4983e2a487e851e6ba742c0307bc
protects <rmanahan@loveyourself.ph>
2026-07-21 14:21:45 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,26 +1,14 @@
 const API_URL =
 "https://script.google.com/macros/s/AKfycbyZr9TUeTYy_mcSRIghXC6xXkXRbB5FRZ8iAoOtZwQqyzhyBVq1nj52SKszXwEsiL7wrw/exec";
 
-async function api(action,data={}){
+async function api(action, data = {}) {
 
-    const response = await fetch(API_URL,{
-
-        method:"POST",
-
-        headers:{
-
-            "Content-Type":"application/json"
-
-        },
-
-        body:JSON.stringify({
-
+    const response = await fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify({
             action,
-
             ...data
-
         })
-
     });
 
     return await response.json();
